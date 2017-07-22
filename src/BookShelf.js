ee1539a65ab67ffd6c0310888e3e2ebd40a35013
Daniele <daniele.rb6@gmail.com>
2017-07-22 15:28:36 +0200

    refactor: Extract a Shelf Component

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -7,135 +7,34 @@ function BookShelf(props) {
         <h1>MyReads</h1>
       </div>
       <div className="list-books-content">
-        <div>
-          <div className="bookshelf">
-            <h2 className="bookshelf-title">Currently Reading</h2>
-            <div className="bookshelf-books">
-              <ol className="books-grid">
-              	{props.books.filter((book) => (
-              		book.shelf === 'currentlyReading'
-              		)).map((book) => (
-              			console.log(book.title),
-
-                    <li key={book.id}>
-                      <Book book={book} />
-                    </li>
-
-              			// <li key={book.id}>
-  	              //     <div className="book">
-  	              //       <div className="book-top">
-  	              //         <div className="book-cover" style={{
-                 //            width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})`
-                 //          }}/>
-
-  	              //         <div className="book-shelf-changer">
-  	              //           <select>
-  	              //             <option value="none" disabled>Move to...</option>
-  	              //             <option value="currentlyReading" selected="selected">Currently Reading</option>
-  	              //             <option value="wantToRead">Want to Read</option>
-  	              //             <option value="read">Read</option>
-  	              //             <option value="none">None</option>
-  	              //           </select>
-  	              //         </div>
-  	              //       </div>
-  	              //       <div className="book-title">{book.title}</div>
-  	              //       <div className="book-authors">{book.authors[0]}</div>
-  	              //     </div>
-  	              //   </li>
-                  )
-                )}
-              </ol>
-            </div>
-          </div>
-        </div>
-
-          <div className="bookshelf">
-            <h2 className="bookshelf-title">Want to Read</h2>
-            <div className="bookshelf-books">
-              <ol className="books-grid">
-                {props.books.filter((book) => (
-                  book.shelf === 'wantToRead'
-                  )).map((book) => (
-                    console.log(book.title),
-
-                    <li key={book.id}>
-
-                      <Book book={book} />
-
-                    </li>
-
-                    // <li key={book.id}>
-                    //   <div className="book">
-                    //     <div className="book-top">
-                    //       <div className="book-cover" style={{
-                    //         width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})`
-                    //       }}/>
-
-                    //       <div className="book-shelf-changer">
-                    //         <select>
-                    //           <option value="none" disabled>Move to...</option>
-                    //           <option value="currentlyReading">Currently Reading</option>
-                    //           <option value="wantToRead">Want to Read</option>
-                    //           <option value="read">Read</option>
-                    //           <option value="none">None</option>
-                    //         </select>
-                    //       </div>
-                    //     </div>
-                    //     <div className="book-title">{book.title}</div>
-                    //     <div className="book-authors">{book.authors[0]}</div>
-                    //   </div>
-                    // </li>
-                  )
-                )}
-              </ol>
-            </div>
-          </div>
-
-          <div className="bookshelf">
-            <h2 className="bookshelf-title">Read</h2>
-            <div className="bookshelf-books">
-              <ol className="books-grid">
-                {props.books.filter((book) => (
-                  book.shelf === 'read'
-                  )).map((book) => (
-                    console.log(book.title),
-
-                    <li key={book.id}>
-
-                      <Book book={book} />
-
-                    </li>
-
-                    // <li key={book.id}>
-                    //   <div className="book">
-                    //     <div className="book-top">
-                    //       <div className="book-cover" style={{
-                    //         width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})`
-                    //       }}/>
-
-                    //       <div className="book-shelf-changer">
-                    //         <select>
-                    //           <option value="none" disabled>Move to...</option>
-                    //           <option value="currentlyReading">Currently Reading</option>
-                    //           <option value="wantToRead">Want to Read</option>
-                    //           <option value="read">Read</option>
-                    //           <option value="none">None</option>
-                    //         </select>
-                    //       </div>
-                    //     </div>
-                    //     <div className="book-title">{book.title}</div>
-                    //     <div className="book-authors">{book.authors[0]}</div>
-                    //   </div>
-                    // </li>
-                  )
-                )}
-              </ol>
-            </div>
-          </div>
-
-        </div>
+        <Shelf title="Currently Reading" shelf="currentlyReading" books={props.books} />
+        <Shelf title="Want To Read" shelf="wantToRead" books={props.books} />
+        <Shelf title="Read" shelf="read" books={props.books} />
       </div>
+    </div>
 	)
+}
+
+function Shelf(props) {
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{props.title}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {props.books.filter((book) => (
+            book.shelf === props.shelf
+            )).map((book) => (
+              console.log(book.title),
+
+              <li key={book.id}>
+                <Book book={book} />
+              </li>
+            )
+          )}
+        </ol>
+      </div>
+    </div>
+  )
 }
 
 function Book(props) {
