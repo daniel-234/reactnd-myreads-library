b783fed3a6ba41d833774c129c802049763beab8
Daniele <daniele.rb6@gmail.com>
2017-07-24 12:34:35 +0200

    refactor: Delete old code

--- src/App.js
+++ src/App.js
@@ -40,6 +40,7 @@ class BooksApp extends React.Component {
     }))
   }
 
+  // Change state to show shelf page.
   showBookShelf = () => (
     this.setState({ showSearchPage: false })
   )
@@ -52,30 +53,11 @@ class BooksApp extends React.Component {
     })
   }
 
+  // Render screen based on state.
   render() {
     return (
       <div className="app">
         {this.state.showSearchPage ? (
-          // <div className="search-books">
-          //   <div className="search-books-bar">
-          //     <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-          //     <div className="search-books-input-wrapper">
-          //       {
-          //         NOTES: The search from BooksAPI is limited to a particular set of search terms.
-          //         You can find these search terms here:
-          //         https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-          //         However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-          //         you don't find a specific author or title. Every search is limited by search terms.
-          //       }
-          //       <input type="text" placeholder="Search by title or author"/>
-
-          //     </div>
-          //   </div>
-          //   <div className="search-books-results">
-          //     <ol className="books-grid"></ol>
-          //   </div>
-          // </div>
           <SearchBook backToBooks={this.showBookShelf} />
         ) : (
           <div>
