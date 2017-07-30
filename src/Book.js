b4734c1fdb1794c221d04ed3e0b4c10157bb6d8b
Daniele <daniele.rb6@gmail.com>
2017-07-30 16:07:34 +0200

    fix: Delete Book component status
    State in the Book Component had a property that was assigned `Props` as value. Removed it as it is an anti-pattern in React, because it was changing the immutable nature of `Props` to a mutable value, that is what state is.

--- src/Book.js
+++ src/Book.js
@@ -4,13 +4,6 @@ import SelectList from './SelectList.js'
 // The 'Book' Component has a 'book' property which is one of the object
 // items returned by the API.
 class Book extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: this.props
-    };
-  }
-
   // Call the `onMove` function in its props.
   changeShelf = (s, book) => {
     this.props.onMove(s, book);
@@ -21,18 +14,18 @@ class Book extends Component {
       <div className="book">
         <div className="book-top">
           <div className="book-cover" style={{
-            width: 128, height: 193, backgroundImage: `url(${this.state.value.book.imageLinks.smallThumbnail})`
+            width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks.smallThumbnail})`
           }}/>
           <div className="book-shelf-changer">
-            <SelectList book={this.state.value.book} onChangeShelf={this.changeShelf} />
+            <SelectList book={this.props.book} onChangeShelf={this.changeShelf} />
           </div>
         </div>
-        <div className="book-title">{this.state.value.book.title}</div>
+        <div className="book-title">{this.props.book.title}</div>
         <div className="book-authors">
           {/*
             Loop over the authors array and display each of its elements as a separate div.
           */}
-          {this.state.value.book.authors.map((a) => (
+          {this.props.book.authors.map((a) => (
             <div key={a}>
               {a}
             </div>
